b011851bcdd3b3e98c90fdd0622cc571bd2ec360
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2022-09-09 23:17:05 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 	int i;
 	int j;
 
-	for(i =0; i <= 99; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for(j = i + 1; j <=99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 
-			if(!(i == 99 && j == 99))
+			if (!(i == 99 && j == 99))
 			{
 				putchar(',');
 				putchar(' ');
@@ -28,5 +28,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
